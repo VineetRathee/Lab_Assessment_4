c4d9ea21275e4568b8f8ddb20a1a813f528d4b2c
VineetRathee <40818026+VineetRathee@users.noreply.github.com>
2021-03-09 19:53:14 +0530

    Delete src/main/java/com/bookapp directory

--- src/main/java/com/bookapp/exceptions/DataAccessException.java
+++
@@ -1,9 +0,0 @@
-package com.bookapp.exceptions;
-
-public class DataAccessException extends RuntimeException{
-
-	private static final long serialVersionUID = 1L;
-	public DataAccessException(String message) {
-		super(message);
-	}
-}
